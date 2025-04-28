47cfc74f43a24e46604f9547db68f355fc9a7d7c
BB-fat <1056871944@qq.com>
2025-04-28 11:46:45 +0800

    Add async support for MDQuery with new query_async module and update Cargo.toml for async features

--- src/apple/mod.rs
+++ src/apple/mod.rs
@@ -4,7 +4,13 @@ mod item;
 mod model;
 mod query;
 
+#[cfg(feature = "async")]
+mod query_async;
+
 pub use builder::*;
 pub use item::*;
 pub use model::*;
 pub use query::*;
+
+#[cfg(feature = "async")]
+pub use query_async::*;
